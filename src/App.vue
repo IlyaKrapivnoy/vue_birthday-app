53598088e3337c32e9551ea28f89051f5b060553
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-29 18:33:32 +0100

    add choosing dates input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <h1>Birthday App</h1>
+
+  <div>
+    <label for="dateInput">Select a Date:</label>
+    <input
+      id="dateInput"
+      type="date"
+      v-model="selectedDate"
+      @change="handleDateChange"
+    />
+    <p>Selected Date: {{ selectedDate }}</p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const selectedDate = ref('');
+
+const handleDateChange = () => {
+  console.log('Selected Date:', selectedDate.value);
+};
+</script>
 
 <style scoped></style>
