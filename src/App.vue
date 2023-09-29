94e9bfed24013885001d2962064dddf956190cb9
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-29 18:41:13 +0100

    limit date range

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,10 @@
       type="date"
       v-model="selectedDate"
       @change="handleDateChange"
+      :max="getCurrentDate()"
     />
     <p>Selected Date: {{ selectedDate }}</p>
-    <p>Your Age: {{ calculatedAge }} years</p>
+    <p>Your Age: {{ calculatedAge || 0 }} years</p>
   </div>
 </template>
 
@@ -30,6 +31,12 @@ const calculatedAge = computed(() => {
   const ageInYears = ageInMilliseconds / (365 * 24 * 60 * 60 * 1000);
   return Math.floor(ageInYears);
 });
-</script>
 
-<style scoped></style>
+const getCurrentDate = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+</script>
