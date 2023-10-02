d12495c49ef9b00be29b3eb0a4b23e2d78090a9f
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-10-02 18:20:54 +0100

    remove current age

--- src/App.vue
+++ src/App.vue
@@ -23,34 +23,41 @@
         </div>
 
         <div class="text-gray-300">
-          Time until your next birthday:
-          <span class="text-gray-800 font-bold">{{
-            timeUntilNextBirthday
-          }}</span>
+          Time until your next birthday (days):
+          <div
+            class="text-gray-800 font-bold"
+            v-if="age.months === 0 && age.days === 0"
+          >
+            Happy B-Day!
+          </div>
+          <div v-else>
+            <span id="countdownDays">{{ countdownDays }}</span> days
+          </div>
         </div>
-      </div>
 
-      <div class="mt-20">
-        <h2 class="text-gray-300">Your Current Age:</h2>
-        <p v-if="showAgeCalculation" class="text-3xl text-white">
-          Calculating<span class="animate-ping">...</span>
-        </p>
-        <p v-else class="text-3xl text-white">
-          {{ age.years }} years, {{ age.months }} months, {{ age.days }} days,
-          {{ age.hours }} hours, {{ age.minutes }} minutes,
-          {{ age.seconds }} seconds
-        </p>
+        <div class="text-gray-300">
+          Time until your next birthday (milliseconds):
+          <div
+            class="text-gray-800 font-bold"
+            v-if="age.months === 0 && age.days === 0"
+          >
+            Happy B-Day!
+          </div>
+          <div v-else>
+            <span id="countdownMilliseconds">{{ countdownMilliseconds }}</span>
+            milliseconds
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount, computed } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 const selectedDate = ref(localStorage.getItem('selectedDate') || '');
 const selectedTime = ref(localStorage.getItem('selectedTime') || '');
-const showAgeCalculation = ref(true);
 
 const handleDateChange = () => {
   localStorage.setItem('selectedDate', selectedDate.value);
@@ -71,41 +78,26 @@ const age = {
   seconds: ref(0)
 };
 
-const updateAge = () => {
-  const timerInterval = setInterval(() => {
-    const birthDate = new Date(
-      selectedDate.value + 'T' + selectedTime.value + ':00'
-    );
-    const currentDate = new Date();
-    const ageInMilliseconds = currentDate - birthDate;
+const countdownMilliseconds = computed(() => {
+  const today = new Date();
+  const selectedDateTime = new Date(
+    selectedDate.value + 'T' + selectedTime.value + ':00'
+  );
+  const nextBirthday = new Date(
+    today.getFullYear(),
+    selectedDateTime.getMonth(),
+    selectedDateTime.getDate()
+  );
 
-    const ageDate = new Date(ageInMilliseconds);
+  if (nextBirthday < today) {
+    nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+  }
 
-    const years = ageDate.getUTCFullYear() - 1970;
-    const months = ageDate.getUTCMonth();
-    const days = ageDate.getUTCDate() - 1;
+  return nextBirthday - today;
+});
 
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
-
-    age.years.value = years;
-    age.months.value = months;
-    age.days.value = days;
-    age.hours.value = hours;
-    age.minutes.value = minutes;
-    age.seconds.value = seconds;
-
-    showAgeCalculation.value = false;
-  }, 1000);
-
-  onBeforeUnmount(() => {
-    clearInterval(timerInterval);
-  });
-};
-
-onMounted(() => {
-  updateAge();
+watch(age, () => {
+  countdownMilliseconds.value = 0;
 });
 
 const getCurrentDate = () => {
@@ -116,11 +108,7 @@ const getCurrentDate = () => {
   return `${year}-${month}-${day}`;
 };
 
-const timeUntilNextBirthday = computed(() => {
-  if (age.months.value === 0 && age.days.value === 0) {
-    return 'Happy B-Day!';
-  }
-
+const countdownDays = computed(() => {
   const today = new Date();
   const selectedDateTime = new Date(
     selectedDate.value + 'T' + selectedTime.value + ':00'
@@ -136,13 +124,6 @@ const timeUntilNextBirthday = computed(() => {
   }
 
   const timeDifference = nextBirthday - today;
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 });
 </script>
